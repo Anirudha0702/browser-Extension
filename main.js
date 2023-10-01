85b73ade2327d31a01202969941d5a22f7b372cc
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-10-01 19:57:43 +0530

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+const root=document.getElementById("root");
+const url="https://api.jikan.moe/v4/seasons/now";
+fetch(url)
+.then((response)=>response.json())
+.then((data)=>{
+    const animes=data.data;
+    animes.forEach(anime => {
+        console.log(anime.images?.jpg?.image_url);
+        root.innerHTML+=`
+        <div class="anime_info_wrapper">
+            <div class="anime_img_wrapper">
+                <img src=${anime.images?.jpg?.image_url} alt="anime Image">
+            </div>
+            <span class="anime_title">${anime.title}</span>
+        </div>`;
+    });
+})
